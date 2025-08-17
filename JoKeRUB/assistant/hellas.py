5cd755fe7588468e1533dbd93cf4e418418789b7
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-08-17 09:38:07 -0700

    Update hellas.py

--- JoKeRUB/assistant/hellas.py
+++ JoKeRUB/assistant/hellas.py
@@ -9,7 +9,7 @@ PYTHON_VERSION = "3.9.6"
 OWNER_USER = "@F_Q_1"
 SUPPORT_GROUP = "https://t.me/HELLASSupport"
 SOURCE_CHANNEL = "https://t.me/HELLASUserBot"
-
+UNAUTHORIZED_TEXT = "انت لا تستطيع استخدام البوت احصل على بوتك من HELLASUserBot"
 # روابط البوت والمطور
 SOURCE_NAME = "تنصيب البوت"
 DEVELOPER_NAME = "مطور البوت"
