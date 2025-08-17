f30db9d0f7224b440d50da9ff78119295418a518
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-08-17 04:16:27 -0700

    Create hellas.py

---
+++ JoKeRUB/plugins/hellas.py
@@ -0,0 +1,19 @@
+# WRITE BY JoKeRUB
+# PLUGIN FOR JoKeRUB
+# @HELLASUserBot
+
+OWNER = "👑 ᴏᴡɴᴇʀ : [@F_Q_1](https://t.me/F_Q_1)"
+BOT = "🤖 BOT   : [@qvxbot](https://t.me/qvxbot)"
+CHANNEL = "📡 CH    : [@HELLASUserBot](https://t.me/HELLASUserBot)"
+
+def get_rights():
+    return (
+        "╭──── • ◈ • ────╮\n"
+        f"│ {OWNER}\n"
+        f"│ {BOT}\n"
+        f"│ {CHANNEL}\n"
+        "╰──── • ◈ • ────╯"
+    )
+
+def ch_tag():
+    return "⌔︙CH : @HELLASUserBot"
