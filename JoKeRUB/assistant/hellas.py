1f41c54d01ca7b50427306cef0e1f71d5979e1ce
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-08-17 09:28:42 -0700

    Update hellas.py

--- JoKeRUB/assistant/hellas.py
+++ JoKeRUB/assistant/hellas.py
@@ -1,8 +1,7 @@
 # hellas.py
 # ─────────────────────────────
 #    جميع الحقوق محفوظة لمطوري سورس جـيبثون حصرياً
-#    اذا تخمط الملف اذكر الحقوق وكاتبيه ومطوريه لا تحذف الحقوق
-#    ─────────────────────────────
+# ─────────────────────────────
 
 BOT_NAME = "HELLAS 𝘜𝘚𝘌𝘙𝘉𝘖𝘛"
 SOURCE_VERSION = "1.23.0"
@@ -11,6 +10,18 @@ OWNER_USER = "@F_Q_1"
 SUPPORT_GROUP = "https://t.me/HELLASSupport"
 SOURCE_CHANNEL = "https://t.me/HELLASUserBot"
 
+# روابط البوت والمطور
+SOURCE_NAME = "تنصيب البوت"
+DEVELOPER_NAME = "مطور البوت"
+DEVELOPER_LINK = "https://t.me/F_Q_1"
+
+INSTALL_NAME = "رابط التنصيب"
+INSTALL_LINK = "https://t.me/HELLASUserBot?startinstall=true"
+
+SUPPORT_NAME = "مجموعة الدعم"
+SUPPORT_LINK = "https://t.me/HELLASSupport"
+
+# نص الترحيب
 HELLO_TEXT = f"""
 **{BOT_NAME}**
 -━═━═━═━═━━═━═━═━═━-
@@ -18,9 +29,12 @@ HELLO_TEXT = f"""
 **- اصدار السورس **: {SOURCE_VERSION}
 **- اصدار البايثون **: {PYTHON_VERSION}
 **- القناة **: {SOURCE_CHANNEL}
--━═━═━═━═━━═━═━═━═━-
+-━═━━═━═━━━═━═━═━═━-
 """
 
-# دالة ترجع النصوص
+# دالة ترجع الحقوق
 def get_rights():
     return f"حقوق السورس لـ {BOT_NAME} | Dev : {OWNER_USER} "
+
+# استيراد للملفات الأخرى عند الحاجة
+
