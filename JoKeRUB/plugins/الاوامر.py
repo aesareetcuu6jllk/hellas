548bad013bda2aa715360b131248533c7fcc4972
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-08-19 02:21:03 -0700

    Update الاوامر.py

--- JoKeRUB/plugins/الاوامر.py
+++ JoKeRUB/plugins/الاوامر.py
@@ -17,7 +17,7 @@ from ..core.managers import edit_or_reply
 from ..sql_helper.globals import gvarstatus
 
 plugin_category = "extra"
-from config import NAME, CH
+from coonfig import NAME, CH
 rehu = [
     "اللهم صلِ على محمد و على آله محمد",
 ]
